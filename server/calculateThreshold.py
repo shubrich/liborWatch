9b4651b8d4cd0363726875ab4a3955acb350d4d6
Sebastian Hubrich <sebastian.hubrich@gmail.com>
2018-03-23 11:48:02 +0100

    Update to Python 3.x

--- server/calculateThreshold.py
+++ server/calculateThreshold.py
@@ -5,8 +5,6 @@ import smtplib
 import datetime as dt
 from datetime import datetime, timedelta
 from calendar import monthrange
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
 
 def sendMail(recipient, subject, body):
 	print('test')
@@ -27,19 +25,19 @@ def sendMail(recipient, subject, body):
 
 
 def monthdelta(d1, d2):
-    delta = 0
-    while True:
-        mdays = monthrange(d1.year, d1.month)[1]
-        d1 += timedelta(days=mdays)
-        if d1 <= d2:
-            delta += 1
-        else:
-            break
-    return delta
+	delta = 0
+	while True:
+		mdays = monthrange(d1.year, d1.month)[1]
+		d1 += timedelta(days=mdays)
+		if d1 <= d2:
+			delta += 1
+		else:
+			break
+	return delta
 
 
 def percentageOf(part, whole):
-  return 100.0 * part/whole
+	return 100.0 * part/whole
 
 
 def calculateThresholds():
@@ -48,20 +46,20 @@ def calculateThresholds():
 	con = sqlite3.connect('liborWatch.sqlite')
 	with con:
 		cur = con.cursor()
-		
+
 		# Get the libor rates first
 		cur.execute("SELECT * FROM LiborRate WHERE ID = (SELECT MAX(ID) FROM LiborRate)")
 		liborResult = cur.fetchone()
 		libor3Months = liborResult[2]
 		libor6Months = liborResult[3]
-		
+
 		cur.execute("SELECT Mortgage.id, Mortgage.fk_customer, Mortgage.bank, Mortgage.mortgageSum, Mortgage.liborMargin, Mortgage.discount, Mortgage.yearlyAmortization, Mortgage.startDate, Mortgage.interestPaid, Mortgage.planningHorizon, Mortgage.maxInterestOverHorizon, Mortgage.lastInterestPaidTS, Mortgage.lastMaxInterestResult, Mortgage.liborMonths, Customer.email FROM Mortgage INNER JOIN Customer ON Mortgage.fk_customer = Customer.id")
 		mortgageResults = cur.fetchall()
 		for mortgage in mortgageResults:
 			fixedRate = []
 			cur.execute("SELECT * FROM BankRate WHERE bankName = ? AND date = ? ORDER BY fixedForYears", [mortgage[2], today])
 			bankRateResults = cur.fetchall()
-			
+
 			for bankRate in bankRateResults:
 				fixedRate.append(bankRate[4])
 
@@ -73,14 +71,14 @@ def calculateThresholds():
 			planningHorizon = mortgage[9] # Years
 			maxInterestOverHorizon = mortgage[10] # Percent
 			lastInterestPaidTS = mortgage[11]
-			
+
 			maxInterestCost = mortgageSum * planningHorizon * (maxInterestOverHorizon / 100)
 			startDate = datetime.strptime(mortgage[7], '%Y-%m-%d')
 #			startDate = datetime.strptime('2014-01-25', '%Y-%m-%d')
 			monthsSinceStartDate = monthdelta(startDate, dt.datetime.now())
 			remainingMonthsInPlanningHorizon = ((planningHorizon * 12)) - monthsSinceStartDate
 			residualDebt = mortgageSum - ((yearlyAmortization / 12) * monthsSinceStartDate)
-			
+
 			if lastInterestPaidTS is not None:
 				lastInterestPaidTSDate = datetime.strptime(lastInterestPaidTS, '%Y-%m-%d')
 				monthsSinceLastInterestPaidTS = monthdelta(lastInterestPaidTSDate, dt.datetime.now())
@@ -88,62 +86,62 @@ def calculateThresholds():
 					# TODO: Check if we have a 3 or 6 month libor
 					newInterest = (residualDebt * ((libor3Months + liborMargin) / 100) / 12) * monthsSinceStartDate
 					interestPaid += newInterest
-					
-					cur.execute("UPDATE Mortgage SET interestPaid=?, lastInterestPaidTS=? WHERE Id=?", (interestPaid, today, mortgageID))        
-    				con.commit()
+
+					cur.execute("UPDATE Mortgage SET interestPaid=?, lastInterestPaidTS=? WHERE Id=?", (interestPaid, today, mortgageID))
+					con.commit()
 			else:
 				if monthsSinceStartDate > 0:
 					# TODO: Check if we have a 3 or 6 month libor
 					newInterest = (residualDebt * ((libor3Months + liborMargin) / 100) / 12) * monthsSinceStartDate
 					interestPaid += newInterest
-					
-					cur.execute("UPDATE Mortgage SET interestPaid=?, lastInterestPaidTS=? WHERE Id=?", (interestPaid, today, mortgageID))        
-    				con.commit()
-			
+
+					cur.execute("UPDATE Mortgage SET interestPaid=?, lastInterestPaidTS=? WHERE Id=?", (interestPaid, today, mortgageID))
+					con.commit()
+
 			lastMaxInterestResult = percentageOf((maxInterestCost - interestPaid) / (remainingMonthsInPlanningHorizon / 12.0), residualDebt)
-			cur.execute("UPDATE Mortgage SET lastMaxInterestResult=? WHERE Id=?", (lastMaxInterestResult, mortgageID))        
+			cur.execute("UPDATE Mortgage SET lastMaxInterestResult=? WHERE Id=?", (lastMaxInterestResult, mortgageID))
 			con.commit()
-			
+
 			# Warning mechanism if lastMaxInterestResult is close (or over) to the interest rate
 			# for a fixed mortgage that spans the remainingMonthsInPlanningHorizon
-			fixedMortgageTime = remainingMonthsInPlanningHorizon / 12
+			fixedMortgageTime = remainingMonthsInPlanningHorizon // 12
 
 			if (len(fixedRate) < fixedMortgageTime):
 				subject = "liborWatch - ERROR"
 				message = "There are only {0} fixed mortgage rates but we need {1}".format(len(fixedRate), fixedMortgageTime)
 				sendMail(mortgage[14], subject, message)
 				return
-			
+
 			matchingFixedRate = fixedRate[fixedMortgageTime - 1]
 			if matchingFixedRate == -100:
 				subject = "liborWatch - ERROR"
 				message = "The matching fixed rate ({0} year(s)) doesn't seem to be available from this bank or we don't have rate information.".format(fixedMortgageTime)
 				sendMail(mortgage[14], subject, message)
 				return
-			
+
 	interestDifference = lastMaxInterestResult - matchingFixedRate
 	if interestDifference > 0.3:
 		print('interestDifference = ' + str(interestDifference))
 		return
 
 	subject = "liborWatch"
- 	if interestDifference <= 0.3 and interestDifference > 0.2:
- 		subject = "liborWatch - INFO"
+	if interestDifference <= 0.3 and interestDifference > 0.2:
+		subject = "liborWatch - INFO"
 
 	if interestDifference <= 0.2 and interestDifference > 0.1:
- 		subject = "liborWatch - WARN"
+		subject = "liborWatch - WARN"
 
 	if interestDifference <= 0.1:
- 		subject = "liborWatch - WARN"
+		subject = "liborWatch - WARN"
 
 	message = "The rate for {0} years fixed mortgages ({1}%) approaches our threshold of {2}% with only {3}% difference.\r\nThe current libor rate is at {4}%".format(fixedMortgageTime, matchingFixedRate, lastMaxInterestResult, interestDifference, libor3Months)
 	sendMail(mortgage[14], subject, message)
 
-	
-def main():   
+
+def main():
 	calculateThresholds()
 
 
 if __name__ == "__main__":
-    main()
+	main()
 
--- server/plugins/raiffeisenPlugin.py
+++ server/plugins/raiffeisenPlugin.py
@@ -1,16 +1,16 @@
 from yapsy.IPlugin import IPlugin
 import logging
 import json
-import urllib2
+from urllib.request import urlopen
 
 log = logging.getLogger('yapsy')
 
 class RaiffeisenPlugin(IPlugin):
 	def getRates(self):
 		rates = []
-		rates.append(-100) # No 1-year fixed 
+		rates.append(-100) # No 1-year fixed
 
-		json_data = json.load(urllib2.urlopen('https://www.raiffeisen.ch/apiservices/www-mortgage-services/rest/products/1344'))
+		json_data = json.load(urlopen('https://www.raiffeisen.ch/apiservices/www-mortgage-services/rest/products/1344'))
 		for entry in json_data['products']['product']:
 			if(entry['baseProperties']['model'] == 'FIXED'):
 				# Found the fixed mortgages, now iterate the variants
@@ -18,7 +18,7 @@ class RaiffeisenPlugin(IPlugin):
 				for variant in variants:
 					rate = variant['rate'][0]['interestRate']
 					rates.append(rate)
-				
+
 				break;
-			
+
 		return rates
